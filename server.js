5cb3ba2388f140cb4d1847f48244aa7938ed29c9
Richard Bell <rtb@getadministrate.com>
2017-08-05 22:04:06 +0100

    Use seperate html file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -9,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.sendFile(path.join(__dirname+'/app/index.html'));
 });
 
 app.listen(PORT, HOST);
